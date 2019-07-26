e9b639d7823e52634d4006eee20674a75bbcc1e6
prakspc01 <prakspc01@i82sn05.irf.uni-karlsruhe.de>
2019-07-26 19:06:42 +0200

    Refactored for better Benchmarking.

--- projekt2/Aufgabe3/runGPU.sh
+++ projekt2/Aufgabe3/runGPU.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-THREAD_COUNT=8
+THREAD_COUNT=3
 #NAME=gaussSeidelGPU
 NAME=gaussSeidelGPUasync
 CODE=$NAME.cu
-SIZE=4
+SIZE=6
+ITER=25
 DEF_NAME1=L
 DEF_NAME2=PRINT
+DEF_NAME3=ITERATIONS
 
 export OMP_NUM_THREADS=$THREAD_COUNT
 
@@ -14,11 +16,14 @@ echo "Compiling"
 echo "#########"
 rm $NAME
 #nvcc -D$DEF_NAME1=$SIZE -D$DEF_NAME2 $CODE -o $NAME -Xcompiler -fopenmp
-nvcc -D$DEF_NAME1=$SIZE $CODE -o $NAME -Xcompiler -fopenmp
+nvcc -D$DEF_NAME1=$SIZE -D$DEF_NAME3=$ITER $CODE -o $NAME -Xcompiler -fopenmp
 
-start=`date +%s%N`
-./$NAME
-end=`date +%s%N`
+echo "THREAD = ${THREAD_COUNT}, SIZE = ${SIZE}, ITER = ${ITER}"
+
+
+./$NAME | grep "Time used\|Took at least"
+./$NAME | grep "Time used\|Took at least"
+./$NAME | grep "Time used\|Took at least"
 exit 0
 echo 1. Execution time was $end - $start = `expr $end - $start` nanoseconds.
 start=`date +%s%N`
