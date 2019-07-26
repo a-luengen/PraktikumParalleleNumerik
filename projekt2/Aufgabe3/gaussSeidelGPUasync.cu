9995b145d3f44785995d2762d144889c4e8904b6
prakspc01 <prakspc01@i82sn02.irf.uni-karlsruhe.de>
2019-07-26 14:40:53 +0200

    Current status without embedding for async block calculation.

--- projekt2/Aufgabe3/gaussSeidelGPUasync.cu
+++ projekt2/Aufgabe3/gaussSeidelGPUasync.cu
@@ -17,69 +17,77 @@ void calculateError(float *old_val, float *new_val, int dim, float *result);
 void printVectorInBlock(float *vector, int length, int blockLength);
 void checkForError(const char* msg);
 
-const int ITERATE_ON_BLACK = 0;
-const int ITERATE_ON_RED = 1;
-const int THREADS_PER_BLOCK = 32;
+//const int ITERATE_ON_BLACK = 0;
+//const int ITERATE_ON_RED = 1;
+//const int THREADS_PER_BLOCK = 32;
 const int BLOCK_DIMENSION = 8;
 
-__global__ void blockIterationAsyncJacobi(int dim_u, int dim_u_emb, float h, float* u_emb, int iterCount) {
+__global__ void blockIterationAsyncJacobi(int dim_u, float h, float* u, int iterCount) {
 
-    // 1. Load data from global memory into local
-    // load all needed data from global memory
-    // change only values in inner block and 
-    // write only updated values to global Memory
+
     __shared__ float localBlock[BLOCK_DIMENSION + 2][BLOCK_DIMENSION + 2];
 
-    int global_start_i = blockIdx.x * BLOCK_DIMENSION;
-    int global_start_j = blockIdx.y * BLOCK_DIMENSION;
-    int global_end_i = (blockIdx.x + 1) * BLOCK_DIMENSION + 1;
-    int global_end_j = (blockIdx.y + 1) * BLOCK_DIMENSION + 1;
+    int global_start_i = (blockIdx.x * BLOCK_DIMENSION);
+    int global_start_j = (blockIdx.y * BLOCK_DIMENSION);
+    int global_end_i = (blockIdx.x + 1) * BLOCK_DIMENSION;
+    int global_end_j = (blockIdx.y + 1) * BLOCK_DIMENSION;
 
     int global_i = 0;
     int global_j = 0;
     int local_i = 0;
     int local_j = 0;
 
-
-
-    for(int i = threadIdx.x; i < (BLOCK_DIMENSION + 2) * (BLOCK_DIMENSION + 2); i+=blockDim.x) {
-
-        local_i = i % BLOCK_DIMENSION;
-        local_j = (int) ( i / BLOCK_DIMENSION);
-        global_i = global_start_i + local_i;
-        global_j = global_start_j + local_j;
-
-        #ifdef PRINT
-        printf("ThreadIdx = %d, i = %d, local_i = %d, local_j = %d, global_i = %d, global_j = %d", threadIdx.x, i, local_i, local_j, global_i, global_j);
-        #endif
-
-        if(global_i > 0 && global_i < dim_u_emb && global_j > 0 && )
-            localBlock[local_i][local_j] = u_emb[global_i + global_j * dim_u_emb];
-    }
-    __syncthreads();
-
-    /*
-    // 2. do jacobi iterations on local block
     for(int iterations = 0; iterations < iterCount; iterations++) {
 
+        // 1. Load data from global memory into local
+        // load all needed data from global memory
+        // 8x8 inner with neighbor elements around
+        for(int i = threadIdx.x; i < (BLOCK_DIMENSION + 2) * (BLOCK_DIMENSION + 2); i += blockDim.x) {
+
+            local_i = i % (BLOCK_DIMENSION + 2);
+            local_j = (int) ( i / (BLOCK_DIMENSION + 2));
+            global_i = global_start_i + local_i - 1;
+            global_j = global_start_j + local_j - 1;
+
+            if(blockIdx.x == 1 && blockIdx.y == 1) {
+                printf("ThreadIdx = %d, block.x/y = %d/%d i = %d loc_(i, j) = (%d, %d), glo_(i, j) = (%d, %d)\n", threadIdx.x, global_start_i, global_start_j, i, local_i, local_j, global_i, global_j);
+            }
+            if(global_i >= global_start_i && global_i < global_end_i && global_j >= global_start_j && global_j <= global_end_j) {
+                #ifdef PRINT
+                if(blockIdx.x == 1 && blockIdx.y == 1) {
+                    printf("LOAD - ThreadIdx = %d, block.x/y = %d/%d i = %d loc_(i, j) = (%d, %d), glo_(i, j) = (%d, %d)\n", threadIdx.x, global_start_i, global_start_j, i, local_i, local_j, global_i, global_j);
+                }
+                //printf("ThreadIdx = %d, block.x/y = %d/%d i = %d, local_i = %d, local_j = %d, global_i = %d, global_j = %d\n", threadIdx.x, blockIdx.x, blockIdx.y, i, local_i, local_j, global_i, global_j);
+                #endif
+                localBlock[local_i][local_j] = u[global_i + global_j * dim_u];
+            }
+        }
+        __syncthreads();
+
+        #ifdef PRINT
+
+        #endif
+
+        // 2. do jacobi iteration on local block
+        int i, j;
+        
         for(int iter_flag = 0; iter_flag < 2; iter_flag++) {
 
             int threadID = threadIdx.x * 2 + iter_flag;
-            // use index of thread to calculate position in matrix 
-            // to execute computation on
-            int i, j;
-        
-            // 1. Calculate the index's in the matrix a thread has to work on
+
+            // calc the index, a thread has to work on
+            // only in inner block, so add +1 to avoid iteration on neighbour elements
             j = ((int) threadID / BLOCK_DIMENSION) + 1;
-            i = ((int) threadID % BLOCK_DIMENSION + (1 - 2 * iter_flag) * (j % 2) ) + 1;
-        
-            if(i > 0 && i < BLOCK_DIMENSION + 1 && j < BLOCK_DIMENSION - 1 && j > 0) {
+            i = (((int) threadID % BLOCK_DIMENSION) + (1 - 2 * iter_flag) * (j % 2) ) + 1;
+            global_i = global_start_i + i;
+            global_j = global_start_j + j;        
+
+            if(global_i >= 0 && global_i < dim_u && global_j >= 0 && global_j < dim_u) {
 
                 #ifdef PRINT
-                printf("ThreadID = %d, i = %d, j = %d\n", threadID, i, j);
+                //printf("ThreadID = %d|x=%d|y=%d, i = %d, j = %d\n", threadID, blockIdx.x, blockIdx.y, i, j);
                 #endif
-        
-                // 3. calculate new value for u_emb
+
                 float tempSum = 
                     // top element
                     localBlock[i][j - 1] 
@@ -91,17 +99,64 @@ __global__ void blockIterationAsyncJacobi(int dim_u, int dim_u_emb, float h, flo
                     + localBlock[i][j + 1]; 
 
                 // calc new value for u
-                float newU = (h * h * functionF((j_inner / dim_u + 1) * h, (j_inner % dim_u + 1) * h) + tempSum) / 4.0;
+                float newU = (h * h * functionF(global_i * h, global_j * h) + tempSum) / 4.0;
                 // 4. replace old value
                 
                 localBlock[i][j] = newU;
+
+            }
+            __syncthreads();
+        }
+        
+        #ifdef PRINT
+
+        #endif
+
+        // 3. write back to global memory
+        // only updated values in embedded 8x8 block
+        for(int i = threadIdx.x; i < BLOCK_DIMENSION * BLOCK_DIMENSION; i += blockDim.x) {
+
+            local_i = (i % BLOCK_DIMENSION) + 1;
+            local_j = (int) ( i / BLOCK_DIMENSION) + 1;
+            global_i = global_start_i + local_i - 1;
+            global_j = global_start_j + local_j - 1;
+
+            if(global_i >= 0 && global_i <= dim_u && global_j >= 0 && global_j < global_end_j) {
+                #ifdef PRINT
+                if(blockIdx.x == 0 && blockIdx.y == 0) {
+                    //printf("ThreadIdx = %d, block.x/y = %d/%d i = %d loc_(i, j) = (%d, %d), glo_(i, j) = (%d, %d)\n", threadIdx.x, global_start_i, global_start_j, i, local_i, local_j, global_i, global_j);
+                }
+                #endif
+                u[global_i + global_j * dim_u] = localBlock[local_i][local_j];
             }
         }
         __syncthreads();
     }
+    
+    
+    if(threadIdx.x == 0 && blockIdx.x == 0 && blockIdx.y == 0) {
+        printf("\n");
+        for(int i = 0; i < BLOCK_DIMENSION + 2; i++) {
+            for ( int j = 0; j < BLOCK_DIMENSION + 2; j++) {
+                printf(" %f ", localBlock[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    
+    __syncthreads();
+    if(threadIdx.x == 0 && blockIdx.x == 0 && blockIdx.y == 0) {
+        printf("\n");
+        for(int i = 0; i < dim_u; i++) {
+            for ( int j = 0; j < dim_u; j++) {
+                printf(" %f ", u[i + j * dim_u]);
+            }
+            printf("\n");
+        }
+    }
+    
 
-    cudaThreadSynchronize();
-    */
+    __syncthreads();
 }
 
 
@@ -117,50 +172,27 @@ void jaccobi(int n, float fehlerSchranke, float h, float *u)
 {
     //TODO: Timeranfang
     float fehler = fehlerSchranke + 1;
-
-    // embedd vector u for corner case
-    // u(0, y) = u(1, y) = u(x, 0) = u(y, 1) = 0.0
-    int n_emb = n + 2;
-    float *u_emb = allocateSquareMatrix(n_emb * n_emb, 0, n_emb);
-    float *u_emb_new = allocateSquareMatrix(n_emb * n_emb, 0, n_emb);
-
-    for (int i = 0; i < n_emb; i++)
-    {
-        for (int j = 0; j < n_emb; j++)
-        {
-            if (j == 0 || i == 0 || j == n_emb || i == n_emb)
-            {
-                // fill up with edge value
-                u_emb[i + j * n_emb] = 0.0;
-            }
-            else
-            {
-                // copy value from u
-                u_emb[i + j * n_emb] = u[i - 1 + n * j - 1];
-            }
-        }
-    }
+    float *u_new = allocateSquareMatrix(n * n, 0, n);
 #ifdef PRINT
     // print embedded vector u
-    printSquareMatrix(u_emb, n_emb);
-    printSquareMatrix(u_emb_new, n_emb);
+    printSquareMatrix(u_new, n);
 #endif
 
     // allocate device memory
-    float *gpu_u_emb;
-    cudaMalloc((void**)&gpu_u_emb, n_emb * n_emb * sizeof(float));
+    float *gpu_u;
+    cudaMalloc((void**)&gpu_u, n * n * sizeof(float));
     // copy from host to device
-    cudaMemcpy(gpu_u_emb, u_emb, n_emb * n_emb * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(gpu_u, u, n * n * sizeof(float), cudaMemcpyHostToDevice);
     
     // calculate the blocks per dimension
-    int blocksPerDimension = n_emb / BLOCK_DIMENSION + (n_emb % BLOCK_DIMENSION ? 1: 0);
+    int blocksPerDimension = n / BLOCK_DIMENSION + (n % BLOCK_DIMENSION ? 1: 0);
     int threadsPerBlock = (int) (BLOCK_DIMENSION * BLOCK_DIMENSION) / 2 + (((BLOCK_DIMENSION * BLOCK_DIMENSION) % 2) ? 2 : 0) ;
     dim3 numBlocks(blocksPerDimension, blocksPerDimension);
 
     printf("Running with numBlocks: %d, %d - %d Threads / Block.\n", blocksPerDimension, blocksPerDimension, threadsPerBlock);
     // Iterate as long as we do not come below our fehlerSchranke
     int count = 0;
-    int block_iter = 5;
+    int block_iter = 1;
     while (fehlerSchranke < fehler)
     {
         // black iteration
@@ -169,18 +201,22 @@ void jaccobi(int n, float fehlerSchranke, float h, float *u)
         // red iteration
         //redBlackIteration<<<numBlocks, THREADS_PER_BLOCK>>>(n, n_emb, h, gpu_u_emb, ITERATE_ON_RED);
         //cudaDeviceSynchronize();
-        blockIterationAsyncRedBlack<<<numBlocks, threadsPerBlock>>>(n, n_emb, h, gpu_u_emb, block_iter);
+        blockIterationAsyncJacobi<<<numBlocks, threadsPerBlock>>>(n, h, gpu_u, block_iter);
+        
+        
 
         if(count > 1) {
+            checkForError("Some shit happend.");
+            cudaDeviceSynchronize();
             // move result of first iteration onto host (implicitly synchronizing)
-            cudaMemcpy(u_emb_new, gpu_u_emb, n_emb * n_emb * sizeof(float), cudaMemcpyDeviceToHost);
+            cudaMemcpy(u_new, gpu_u, n * n * sizeof(float), cudaMemcpyDeviceToHost);
             
             // calculate error
-            calculateError(u_emb, u_emb_new, n_emb, &fehler);
+            calculateError(u, u_new, n, &fehler);
             // switch pointers
-            float *temp = u_emb;
-            u_emb = u_emb_new;
-            u_emb_new = temp;
+            float *temp = u;
+            u = u_new;
+            u_new = temp;
         }
 
         
@@ -189,28 +225,20 @@ void jaccobi(int n, float fehlerSchranke, float h, float *u)
 
         #ifdef PRINT
         printf("Iteration-Error = %.8f\n", fehler);
-        printSquareMatrix(u_emb_new, n_emb);
-        break;
+        printSquareMatrix(u_new, n);
+        if(count > 1) {
+            break;
+        }
         #endif
     }
     printf("Took %d Iterations to complete.\n", count);
 #ifdef PRINT
     // print embedded vector u
-    printSquareMatrix(u_emb, n_emb);
+    printSquareMatrix(u, n);
 #endif
-
-    // get values out of embedded vector
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            u[i + j * n] = u_emb[i + 1 + (j + 1) * n_emb];
-        }
-    }
-    //freeSquareMatrix(u_emb, n_emb);
-    cudaFree(gpu_u_emb);
-    free(u_emb_new);
-    free(u_emb);
+    freeSquareMatrix(u_emb, n_emb);
+    cudaFree(gpu_u);
+    free(u_new);
 }
 
 int main()
